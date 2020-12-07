7a10ed4088f49cd2a2aea9342990f0df2db63072
Alex <cydoxy@gmail.com>
2020-12-07 19:03:27 +0200

    added population script

--- big_project/interface/models.py
+++ big_project/interface/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Reviews(models.Model):
     description = models.CharField(max_length=300)
 
+    def __str__(self):
+        return f'{self.description[:50]}'
+
 
 class Items(models.Model):
     name = models.CharField(max_length=300)
     reviews = models.ManyToManyField(Reviews)
+
+    def __str__(self):
+        return f'{self.name}'
